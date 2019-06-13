f6420e21aa1924e394daf4ec20b7f71c657caaf8
Stephan Rajaratnam <Stephan.Rajaratnam@mindgeek.com>
2019-06-13 11:19:50 -0400

    Delete user and profile, add delete expirience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,83 @@ router.get('/user/:user_id', async (req, res) => {
   }
 })
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and post
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove profile, user and TODO remove users post
+    await Profile.findOneAndRemove({ user: req.user.id })
+    await User.findByIdAndRemove({ _id: req.user.id })
+    res.json({ msg: 'User has been deleted' })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Compamy is required')
+        .not()
+        .isEmpty(),
+      check('from', 'Date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body
+    const nexExp = { title, company, location, from, to, current, description }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.experience.unshift(nexExp)
+      await profile.save()
+      res.json(profile)
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete profile experience
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    const index = profile.experience.findIndex(exp => exp.id === req.params.exp_id)
+    profile.experience.splice(index, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
