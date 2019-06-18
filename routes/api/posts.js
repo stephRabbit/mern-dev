67be404b8f3c38e999fb78205f294a9f95ee572d
Stephan Rajaratnam <Stephan.Rajaratnam@mindgeek.com>
2019-06-18 10:07:42 -0400

    Likes and unlikes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -106,4 +106,56 @@ router.delete('/:id', auth, async (req, res) => {
   }
 })
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    // Check if user already liked the post
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' })
+    }
+
+    post.likes.unshift({ user: req.user.id })
+    await post.save()
+    res.json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    res.status(500).send('Server error')
+  }
+})
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    // Check if user already liked the post
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post already not liked' })
+    }
+
+    const index = post.likes.findIndex(like => like.user.toString() === req.user.id)
+    post.likes.splice(index, 1)
+
+    await post.save()
+    res.json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    res.status(500).send('Server error')
+  }
+})
+
 module.exports = router
